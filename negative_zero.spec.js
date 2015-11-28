e5d0b61fa9dd38d12f82158cbde12b075a3efeca
Crystal <omaracrystal@gmail.com>
2015-11-27 19:34:43 -0700

    updated tests

--- negative_zero.spec.js
+++ negative_zero.spec.js
@@ -1,17 +1,55 @@
 var isNegativeZero = require('./negative_zero.js');
 
-describe('isNegativeZero()', function() {
+describe ('isNegativeZero', function () {
+  it ("should return false for non-zero numbers", function()
+  {
+    expect(isNegativeZero(1)).toEqual(false);
+    expect(isNegativeZero(-1)).toEqual(false);
+    expect(isNegativeZero(NaN)).toEqual(false);
+  });
 
-  it('0 is not -0', function() {
+  it ("should return false for strings", function()
+  {
+    expect(isNegativeZero("car")).toEqual(false);
+    expect(isNegativeZero('')).toEqual(false);
+    expect(isNegativeZero("-0")).toEqual(false);
+  });
+  it ("should return false for non-numbers", function()
+  {
+    expect(isNegativeZero(null)).toEqual(false);
+    expect(isNegativeZero(undefined)).toEqual(false);
+    expect(isNegativeZero({})).toEqual(false);
+  });
+  it ("should return false for 0", function ()
+  {
     expect(isNegativeZero(0)).toEqual(false);
   });
-  it('-0 is -0', function() {
+  it ("should return true for -0", function ()
+  {
     expect(isNegativeZero(-0)).toEqual(true);
   });
-
 });
 
 
 //JUnit Tests
-// Test.assertEquals(isNegativeZero(0), false, "0 is not -0");
-// Test.assertEquals(isNegativeZero(-0), true, "-0 is -0");
+// describe 'isNegativeZero', ->
+//   it "should return false for non-zero numbers", ->
+//     Test.assertEquals isNegativeZero(1), false, "1 is not -0"
+//     Test.assertEquals isNegativeZero(-1), false, "-1 is not -0"
+//     Test.assertEquals isNegativeZero(NaN), false, "NaN is not -0"
+
+//   it "should return false for strings", ->
+//     Test.assertEquals isNegativeZero("car"), false, "car is not -0"
+//     Test.assertEquals isNegativeZero(''), false, "empty string is not -0"
+//     Test.assertEquals isNegativeZero("-0"), false, "the string '-0' is not -0"
+
+//   it "should return false for non-numbers", ->
+//     Test.assertEquals isNegativeZero(null), false, "null is not -0"
+//     Test.assertEquals isNegativeZero(undefined), false, "undefined is not -0"
+//     Test.assertEquals isNegativeZero({}), false, "empty object is not -0"
+
+//   it "should return false for 0", ->
+//     Test.assertEquals isNegativeZero(0), false, "0 is not -0"
+
+//   it "should return true for -0", ->
+//     Test.assertEquals isNegativeZero(-0), true, "-0 is -0"
