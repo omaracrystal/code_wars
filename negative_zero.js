a09b841363bcba37b3e12d5043fa3a83ffcbc166
Crystal <omaracrystal@gmail.com>
2015-11-27 19:41:20 -0700

    added more solutions

--- negative_zero.js
+++ negative_zero.js
@@ -1,20 +1,35 @@
-function isNegativeZero (number) {
-    if (typeof(number) == "string") {
+function isNegativeZero (x) {
+    if (typeof(x) == "string") {
       return false;
     }
-    if ( number > 0 ) {
+    if ( x > 0 ) {
         return false;
     }
-    if (number < 0) {
+    if ( x < 0) {
         return false;
     }
-    if ( 1 / number === Number.POSITIVE_INFINITY ) {
+    if ( 1 / x === Number.POSITIVE_INFINITY ) {
         return false;
     }
-    if (1 / number === Number.NEGATIVE_INFINITY ) {
+    if (1 / x === Number.NEGATIVE_INFINITY ) {
       return true;
     }
     return false;
+}
+
+function isNegativeZero(x) {
+  return typeof x === "number" && 1/x === -Infinity;
+}
+
+function isNegativeZero(x) {
+  return x === 0 && 1 / x < 0;
+}
+
+function isNegativeZero(x) {
+  if (x === 0 && 1/x === -Infinity) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isNegativeZero;
